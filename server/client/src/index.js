364c9ca439b1797e8bfe4d6bbd3f38e9bafe4685
Karol <mk.bzowski@gmail.com>
2018-12-11 23:29:19 +0100

    retarget to server '/api/*'

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore , applyMiddleware} from 'redux';
 import App from './components/App';
-
+import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 const store = createStore(reducers, {},applyMiddleware());
