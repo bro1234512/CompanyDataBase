b95446d8f36474abaf2ca20a5e67c8bf2fa03ec5
Rafał Chajec <rafalchajec@gmail.com>
2018-12-15 15:39:29 +0100

    create client side of app

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     }
     render(){
     return(
-        <div>
+        <div className="container">
             <BrowserRouter>
                 <div className="container">
                     <Header/>
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -42,7 +42,7 @@ class Header extends Component{
 
         return(
             <nav>
-                <div className=" blue nav-wrapper">
+                <div className=" blue  nav-wrapper">
                     <Link
                         to={this.props.auth ? '/surveys' : '/'}
                         className="left brand-logo"
